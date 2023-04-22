8fc104262d215565d64fdb225333fb107083b0cd
Harish <htiruna@gmail.com>
2023-04-23 09:34:29 +1000

    Making loading spinner bigger

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
+
 import Spinner from '../Spinner';
 
 const Loading: FC = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-transparent">
-      <Spinner className="text-white" size="1.5em" />
+      <Spinner className="text-white" size="2em" />
     </div>
   );
 };
