acfd920f1d4931aeed22ef02ef2dd3a9f1a44871
Harish <htiruna@gmail.com>
2023-04-23 15:29:07 +1000

    Increasing creativity and updating prompt

--- utils/server/makechain.ts
+++ utils/server/makechain.ts
@@ -10,11 +10,7 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
-{context}
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end: {context}
 
 Question: {question}
 Helpful answer in markdown:`;
@@ -24,7 +20,7 @@ export const makeChain = (
   onTokenStream?: (token: string) => void,
 ) => {
   const model = new OpenAI({
-    temperature: 0,
+    temperature: 1,
     modelName: 'gpt-3.5-turbo',
     streaming: Boolean(onTokenStream), // enable streaming
     callbackManager: onTokenStream
