d21dac5c0bde485204afffc27c46dacce86e3f37
Harish <htiruna@gmail.com>
2023-05-15 18:10:18 +1000

    Change loading colour

--- components/Loading/Loading.tsx
+++ components/Loading/Loading.tsx
@@ -5,7 +5,7 @@ import Spinner from '../Spinner';
 const Loading: FC = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-transparent">
-      <Spinner className="text-white" size="2em" />
+      <Spinner className="text-black" size="2em" />
     </div>
   );
 };
