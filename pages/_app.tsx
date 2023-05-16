dd0471c67095cbb8e192fca2217df10f7ef1f886
Harish <htiruna@gmail.com>
2023-05-16 13:30:50 +1000

    Add page loading progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,23 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-import { UserProvider } from '@auth0/nextjs-auth0/client';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import Router from 'next/router';
 
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import NProgress from 'nprogress';
+
+import 'nprogress/nprogress.css';
 import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
+NProgress.configure({ showSpinner: false }); // this will remove the default spinner
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
